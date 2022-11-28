773822df24524a9fe7b5388bd58f98a1f10c839a
Fatma Refaat <fa.hammam95@gmail.com>
2022-11-28 23:16:09 +0200

    minor change

--- src/components/WhyOtoma.js
+++ src/components/WhyOtoma.js
@@ -1,9 +1,14 @@
 import styles from '../assets/styles/WhyOtoma.module.scss'
 import img2 from '../assets/images/img2.png'
+import Rectangle from '../assets/images/Rectangle.png'
+import phone from '../assets/images/phone.png'
+import hourglass from '../assets/images/hourglass.png'
 
 function WhyOtoma() {
     return (
         <div className={styles.container}>
+            <img src={phone} alt="" className={styles.phone} />
+            <img src={hourglass} alt="" className={styles.hourglass} />
             <div className={styles.whyOtoma}>
                 <img src={img2} />
                 <div className={styles.description}>
@@ -13,7 +18,11 @@ function WhyOtoma() {
                         oratio ad ea monstranda deflexerit quae Romae
                         gererentur</p>
                     <button>Find my design</button>
+
                 </div>
+            </div>
+            <div className={styles.footer}>
+                <img src={Rectangle} alt="" />
             </div>
         </div>
     );
